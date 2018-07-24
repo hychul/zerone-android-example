da170d3c94c6514a42d16c394f5aa499c13553ef
hyoungchul.cho <hyoungchul.cho@linecorp.com>
2018-07-24 11:26:19 +0900

    Replace angle with Quaternion

--- app/src/main/java/com/hychul/zerone/component/Transform.java
+++ app/src/main/java/com/hychul/zerone/component/Transform.java
@@ -9,16 +9,14 @@ public final class Transform extends Component {
     public final Vector3 position;
     public final Vector3 scale;
     public final Quaternion rotation;
-    public float angle;
 
     public Transform() {
-        this(0.0f, 0.0f, 1.0f, 1.0f, 0.0f);
+        this(0.0f, 0.0f, 1.0f, 1.0f);
     }
 
-    public Transform(float x, float y, float scaleX, float scaleY, float angle) {
+    public Transform(float x, float y, float scaleX, float scaleY) {
         this.position = new Vector3(x, y, 0);
         this.scale = new Vector3(scaleX, scaleY, 0);
         this.rotation = new Quaternion();
-        this.angle = angle;
     }
 }
