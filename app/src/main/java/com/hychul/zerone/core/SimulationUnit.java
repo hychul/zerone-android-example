26db3475ec04d422aa31365006062358898470d4
hychul <hychome@gmail.com>
2018-07-30 16:43:07 +0900

    Add lifecycle methods

--- app/src/main/java/com/hychul/zerone/core/SimulationUnit.java
+++ app/src/main/java/com/hychul/zerone/core/SimulationUnit.java
@@ -6,5 +6,20 @@ public abstract class SimulationUnit {
 
     // TODO: Scene management
 
+    // Lifecycle
+    public abstract void onCreate();
+
+    public abstract void onStart();
+
+    public void onResume() {
+        scene.onResume();
+    }
+
     public abstract void onSimulate();
+
+    public abstract void onPause();
+
+    public abstract void onStop();
+
+    public abstract void onDestory();
 }
